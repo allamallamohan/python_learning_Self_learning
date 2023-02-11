cd8a31b4d44d06670cf39ffb48b1000863899783
allamallamohan <119502576+allamallamohan@users.noreply.github.com>
2023-02-11 22:09:07 +0530

    Add files via upload

---
+++ 6Numbers.py
@@ -0,0 +1,19 @@
+x=21
+y=-44
+z=21.2222
+p=3j
+q=-124234.34654654
+
+# print(len(x))  # integer has no length
+# print(len(y)) # integer has no length
+# print(len(z)) # integer has no length
+# print(len(p)) # integer has no length
+# print(len(q)) # integer has no length
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(p))
+print(type(q))
+
+
