62016108b6f98a85555013574b780d310826eb7c
allamallamohan <119502576+allamallamohan@users.noreply.github.com>
2023-02-16 20:50:50 +0530

    Add files via upload

---
+++ 8Boolean.py
@@ -0,0 +1,10 @@
+mohan=28
+vinod=26
+kranthi=25
+
+if mohan>=vinod:
+    print("Mohan is Elder")
+else:
+    print("Vinod is elder")
+
+
---
+++ 9Operators.py
@@ -0,0 +1,36 @@
+x=10
+y=3
+z=400
+
+# arthematic operators
+
+p=x+y
+q=z/x
+r=1000/y
+s=1000//y  #removes decimal values
+t=10*x     
+u=10**x     #acts as exponential like 10^3
+v=1%z
+
+print(p)
+print(q)
+print(r)
+print(s)
+print(t)
+print(u)
+print(v)
+
+x+=100  # same as x=x+100
+y-=2  #same as y=y-2
+z*=3 # same as z=z*3
+p/=2 # same as p=p/2
+
+
+
+print(x)
+print(y)
+print(z)
+print(p)
+
+
+
