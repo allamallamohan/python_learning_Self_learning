0748b4a507b49ad89afef91c539058d773bed4c1
allamallamohan <119502576+allamallamohan@users.noreply.github.com>
2023-02-18 12:39:39 +0530

    Add files via upload

---
+++ 10while_loop.py
@@ -0,0 +1,9 @@
+i=0
+while(i<100):
+    print(''' 
+ MOHAN ------------>   
+      
+                                                          SWATHI ------------>                                                    
+    ''')
+i+=1      #while loop never fails this line doesn't consider for while statement
+    #i+=1   #while loop fails this line it consider for while statement when i value receives >= 100      
---
+++ 11for_loop.py
@@ -0,0 +1,19 @@
+
+## Used for List,Tuple,Set,Map,Dictionary,string data types
+
+series=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,]
+
+for x in series:
+    print(x)
+
+##  Used for Range defining alsoas ( start,end)
+
+for y in range(1,2000):
+    print(y)
+
+##  Used for Range with increment as ( start,end,increment value)
+
+for z in range(1,2000,100):
+
+    #print(z)
+    print(x,y,z)  # in x considered last value in list as fixed
